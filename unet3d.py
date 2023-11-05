b2f9581930a9a526b1167ffb92a837cfb42eb10f
KatjaSi <katja.sivertsen@gmail.com>
2023-11-05 01:49:49 +0100

    drop dropout

--- unet3d.py
+++ unet3d.py
@@ -2,20 +2,20 @@ import torch
 import torch.nn as nn
 
 class UNet3D(nn.Module):
-    def __init__(self, in_channels=4, out_channels=3, dropout = 0.0):
+    def __init__(self, in_channels=4, out_channels=3):
         super(UNet3D,self).__init__()
         # Define the encoder (downsampling) path
-        self.encoder = Encoder(in_channels=in_channels, dropout=dropout)
+        self.encoder = Encoder(in_channels=in_channels)
         
         # Define the middle (bottleneck) layer
         self.middle = nn.Sequential(
-            Conv3DBlock(in_channels=256, out_channels=256, dropout=dropout),
-            Conv3DBlock(in_channels=256, out_channels=512, dropout=dropout),
-            UpConv3DBlock(in_channels=512, out_channels=512, dropout=dropout)
+            Conv3DBlock(in_channels=256, out_channels=256),
+            Conv3DBlock(in_channels=256, out_channels=512),
+            UpConv3DBlock(in_channels=512, out_channels=512)
         )
         
         # Define the decoder (upsampling) path
-        self.decoder = Decoder(in_channels=512, out_channels=out_channels, dropout=dropout) 
+        self.decoder = Decoder(in_channels=512, out_channels=out_channels) 
         
 
         
@@ -28,48 +28,44 @@ class UNet3D(nn.Module):
 
 class UpConv3DBlock(nn.Module):
      
-    def __init__(self, in_channels, out_channels, dropout):
+    def __init__(self, in_channels, out_channels):
         super(UpConv3DBlock, self).__init__()
         self.upconv = nn.ConvTranspose3d(in_channels= in_channels, out_channels=out_channels, kernel_size=(2,2,2), stride=2) # deconvolution
         self.bn = nn.BatchNorm3d(out_channels)
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout3d(p=dropout) 
 
     def forward(self, x):
         x = self.upconv(x)
         x = self.bn(x)
         x = self.relu(x)
-        x = self.dropout(x)  
         return x
     
 class Conv3DBlock(nn.Module):
      
-    def __init__(self, in_channels, out_channels, dropout):
+    def __init__(self, in_channels, out_channels):
         super(Conv3DBlock, self).__init__()
         self.conv = nn.Conv3d(in_channels= in_channels, out_channels=out_channels, kernel_size=(3,3,3), padding=1)
         self.bn = nn.BatchNorm3d(out_channels)
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout3d(p=dropout)
 
     def forward(self, x):
         x = self.conv(x)
         x = self.bn(x)
         x = self.relu(x)
-        x = self.dropout(x)  
         return x
     
 
 class Encoder(nn.Module):
-    def __init__(self, in_channels, dropout):
+    def __init__(self, in_channels):
         super(Encoder, self).__init__()
-        self.conv1 = Conv3DBlock(in_channels=in_channels, out_channels=32, dropout=dropout)
-        self.conv2 = Conv3DBlock(in_channels=32, out_channels=64, dropout=dropout)
+        self.conv1 = Conv3DBlock(in_channels=in_channels, out_channels=32)
+        self.conv2 = Conv3DBlock(in_channels=32, out_channels=64)
         self.pool1 = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.conv3 = Conv3DBlock(in_channels=64, out_channels=64, dropout=dropout)
-        self.conv4 = Conv3DBlock(in_channels=64, out_channels=128, dropout=dropout)
+        self.conv3 = Conv3DBlock(in_channels=64, out_channels=64)
+        self.conv4 = Conv3DBlock(in_channels=64, out_channels=128)
         self.pool2 = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.conv5 = Conv3DBlock(in_channels=128, out_channels=128, dropout=dropout)
-        self.conv6 = Conv3DBlock(in_channels=128, out_channels=256, dropout=dropout)
+        self.conv5 = Conv3DBlock(in_channels=128, out_channels=128)
+        self.conv6 = Conv3DBlock(in_channels=128, out_channels=256)
         self.pool3 = nn.MaxPool3d(kernel_size=2, stride=2)
 
     def forward(self, x):
@@ -80,16 +76,16 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, in_channels, out_channels, dropout):
+    def __init__(self, in_channels, out_channels):
         super(Decoder, self).__init__()
-        self.conv1 = Conv3DBlock(in_channels=in_channels, out_channels=512, dropout=dropout) 
-        self.conv2 = Conv3DBlock(in_channels=256+512, out_channels=256, dropout=dropout)
-        self.upconv1 = UpConv3DBlock(in_channels=256, out_channels=256, dropout=dropout)
-        self.conv3 = Conv3DBlock(in_channels=128+256, out_channels=128, dropout=dropout) 
-        self.conv4 = Conv3DBlock(in_channels=128, out_channels=128, dropout=dropout)
-        self.upconv2 = UpConv3DBlock(in_channels=128, out_channels=128, dropout=dropout)
-        self.conv5 = Conv3DBlock(in_channels=64+128, out_channels=64, dropout=dropout) 
-        self.conv6 = Conv3DBlock(in_channels=64, out_channels=64, dropout=dropout)
+        self.conv1 = Conv3DBlock(in_channels=in_channels, out_channels=512) 
+        self.conv2 = Conv3DBlock(in_channels=256+512, out_channels=256)
+        self.upconv1 = UpConv3DBlock(in_channels=256, out_channels=256)
+        self.conv3 = Conv3DBlock(in_channels=128+256, out_channels=128) 
+        self.conv4 = Conv3DBlock(in_channels=128, out_channels=128)
+        self.upconv2 = UpConv3DBlock(in_channels=128, out_channels=128)
+        self.conv5 = Conv3DBlock(in_channels=64+128, out_channels=64) 
+        self.conv6 = Conv3DBlock(in_channels=64, out_channels=64)
         self.final_conv = nn.Conv3d(64, out_channels, kernel_size=1)
 
     def forward(self,x1,x2,x3,x4):
